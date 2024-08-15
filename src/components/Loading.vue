c65811ba8526e1699072c914f53c3c1543014899
Akam <mr.akam.320@gmail.com>
2024-08-15 15:35:20 +0330

    fix loading bug

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -4,8 +4,14 @@ import {loadingProgress} from '../stores/loadingProgress';
 
 <template>
     <div class="fixed top-0 left-0 w-full h-screen bg-white/90 drop-shadow-xl flex justify-center items-center z-10">
-        <div class="p-4 relative after:absolute after:top-0 after:left-0 after:bg-pink-400 after:h-full w-6/12"
-            :class="`after:w-[${loadingProgress.toString()}%]`">
+        <div
+            class="loading p-4 relative after:absolute after:top-0 after:left-0 after:bg-pink-400 after:h-full w-6/12 border border-pink-500 shadow-sm">
         </div>
     </div>
 </template>
+
+<style>
+.loading::after{
+    width: v-bind(loadingProgress + "%");
+}
+</style>
