bcd648a7e6b2c4926bbf858375c15e420ab155fc
Akam <mr.akam.320@gmail.com>
2024-11-03 22:25:25 +0330

    fix cache bug using PWA

--- src/stores/useLoadModelStore.ts
+++ src/stores/useLoadModelStore.ts
@@ -14,20 +14,8 @@ export const useLoadModelStore = defineStore("load model", () => {
             
             loadingShow.value = true;
             
-            console.log("cachedModel");
-            
-            // Try to download the model and cache it in IndexedDB
-            const cachedModel = await downloadAndCacheModel(path ,path);
-            
-            console.log(cachedModel);
-
-            if (cachedModel) {
-                // Convert ArrayBuffer to Blob
-                const blob = new Blob([cachedModel], { type: 'model/gltf-binary' });
-                const url = URL.createObjectURL(blob); // Create a URL from the Blob
-                
-                const loader = new GLTFLoader();
-                loader.load(url, (gltf) => {
+            const loader = new GLTFLoader();
+            loader.load(path, (gltf) => {
                     loadingShow.value = false;
                     scene.add(gltf.scene);
                     resolve(gltf);
@@ -41,10 +29,7 @@ export const useLoadModelStore = defineStore("load model", () => {
                     loadingShow.value = false;
                     reject(error);
                 });
-            } else {
-                loadingShow.value = false;
-                reject(new Error('Failed to load the GLB model.'));
-            }
+
         });
     }
 
--- vite.config.ts
+++ vite.config.ts
@@ -6,48 +6,48 @@ import { VitePWA } from "vite-plugin-pwa"
 export default defineConfig({
   plugins: [
     vue(),
-    // VitePWA({
-    //   registerType: 'autoUpdate',
-    //   devOptions: {
-    //     enabled: true, // Enables the service worker during development
-    //   },
-    //   manifest: {
-    //     name: 'Your App Name',
-    //     short_name: 'App',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: '/icon-192x192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: '/icon-512x512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //     ],
-    //   },
-    //   workbox: {
-    //     maximumFileSizeToCacheInBytes: 100 * 1024 * 1024, // Increase to 100 MB or more
-    //     runtimeCaching: [
-    //       {
-    //         urlPattern: ({ url }) => url.pathname.endsWith('.glb'), // Caching GLB models
-    //         handler: 'CacheFirst', // Serve from cache first
-    //         options: {
-    //           cacheName: 'glb-cache', // Name of the cache for GLB files
-    //           expiration: {
-    //             maxEntries: 10, // Limit the number of models in the cache
-    //             maxAgeSeconds: 60 * 60 * 24 * 30, // Cache models for 30 days
-    //           },
-    //           cacheableResponse: {
-    //             statuses: [0, 200], // Cache responses with these statuses
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   },
-    // }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true, // Enables the service worker during development
+      },
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 100 * 1024 * 1024, // Increase to 100 MB or more
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.endsWith('.glb'), // Caching GLB models
+            handler: 'CacheFirst', // Serve from cache first
+            options: {
+              cacheName: 'glb-cache', // Name of the cache for GLB files
+              expiration: {
+                maxEntries: 10, // Limit the number of models in the cache
+                maxAgeSeconds: 60 * 60 * 24 * 30, // Cache models for 30 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200], // Cache responses with these statuses
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
   server: {
     host: true
